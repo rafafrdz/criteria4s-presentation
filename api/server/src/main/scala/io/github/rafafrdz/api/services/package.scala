33612d09d3b1da8afa02cc47908c97abd8cddaa3
Rafael Fernández <rafaelfernandezortiz@gmail.com>
2024-05-05 09:10:45 +0200

    alpha-16

--- api/server/src/main/scala/io/github/rafafrdz/api/services/package.scala
+++ api/server/src/main/scala/io/github/rafafrdz/api/services/package.scala
@@ -4,17 +4,6 @@ import org.http4s.dsl.impl.{OptionalQueryParamDecoderMatcher, QueryParamDecoderM
 
 package object services {
 
-//  object RateVar extends QueryParamDecoderMatcher[Double]("rate") {
-//    def check(string: String): Option[Double] =
-//      string.toDoubleOption.filterNot(d => 0.0 <= d || d <= 5.0)
-//
-//    override def unapply(params: Map[String, collection.Seq[String]]): Option[Double] =
-//      for {
-//        rateOpt   <- super.unapply(params)
-//        validRate <- check(rateOpt.toString)
-//      } yield validRate
-//  }
-
   object RateVar extends OptionalQueryParamDecoderMatcher[Double]("rate")
   object OwnerVar extends OptionalQueryParamDecoderMatcher[String]("owner")
 }
